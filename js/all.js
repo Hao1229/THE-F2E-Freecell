8aeff49964b0fdf4f6255994b8132aa7b4e54de1
Hao1229 <s0939859391@gmail.com>
2019-07-22 16:09:55 +0800

    正在撰寫拖曳功能待完成

--- js/all.js
+++ js/all.js
@@ -4,7 +4,7 @@ function startGame() {
     //完成區域左上角
     let finishArea = [[], [], [], []];
     //卡片暫放區(每一格只能放一張)右上角
-    let temporaryArea = [];
+    let temporaryArea = [[],[],[],[]];
     //主要遊戲區共8排
     let maingameArea = [
         [], //7張
@@ -16,6 +16,17 @@ function startGame() {
         [], //6張
         [] //6張
     ];
+    /*拖曳效果數值儲存*/
+    let ondragCard = null  // 負責儲存被抓取卡片的數字
+    ondragGroup = null  //負責儲存被抓取卡片所在的排數
+    ondragSection = null //負責儲存被抓取卡片所在的區域
+    ondropCard = null   //負責儲存被堆疊的卡片數字
+    ondropGroup = null  //負責儲存被堆疊的卡片所在排數
+    ondropSection = null //負責儲存被堆疊的卡片所在區域
+    isgamePause = false
+    isFinished = false
+    isTemporary = false
+
     /*隨機發牌設計*/
     function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -29,7 +40,6 @@ function startGame() {
     for (let i = 0; i < 52; i++) {
         pokerArr.push(i + 1)
     }
-
     // let pokerArr =Array.from(new Array(52)).map(function(item,index){
     //     return index+1 
     // })
@@ -69,7 +79,7 @@ function startGame() {
 
 
 
-    /*將牌渲染到畫面上*/
+    /*將發牌渲染到畫面上*/
     let gamingArea = document.getElementById('gamingArea');
 
     //這裡要將7張牌的分成一區，6張牌的分成一區
@@ -91,8 +101,13 @@ function startGame() {
                 cardGroup.className = 'relative w-100'
                 item.forEach(function (el, num) {
                     let oneCard = document.createElement('div');
+                    if (!isgamePause && num + 1 == item.length) {
+                        oneCard.draggable = true;
+                    }
+                    oneCard.card = `${judgeColor(el)} ${el % 13}`;
                     oneCard.className = 'cardArea absolute';
-                    oneCard.card = el;
+                    oneCard.color = judgeColor(el);
+                    oneCard.section = sectionNum;
                     oneCard.group = index;
                     oneCard.style.transition = 'all .3s'
                     oneCard.style.top = '-1000px';
@@ -101,15 +116,108 @@ function startGame() {
                         oneCard.style.top = num * 30 + 'px';
                         oneCard.style.left = '0px'
                     }, index * num * 30)
-                    oneCard.innerHTML = `<img src="pokerimg/card-${judgeColor(el)}-${el % 13}.svg">`
+                    oneCard.innerHTML = `<img src="pokerimg/card-${judgeColor(el)}-${el % 13}.svg" draggable = false class="${judgeColor(el)} ${el % 13}">`
                     cardGroup.appendChild(oneCard);
                 })
                 cardbiggroupPart.appendChild(cardGroup)
             })
             gamingArea.appendChild(cardbiggroupPart);
-        });  
-    }
+        });
+        
+    };
     putCard();
+
+    /*上方完成牌區渲染*/
+    function sortColor(areaNum){
+        if(areaNum == 0){
+            return 'spade'
+        }else if(areaNum == 1){
+            return 'heart'
+        }else if(areaNum == 2){
+            return 'diamond'
+        }else if (areaNum == 3){
+            return 'club'
+        }
+    }   
+    let finished = document.getElementById('finished')
+    function storefinishCard() {
+        finishArea.forEach(function (item, index) {
+            let completedDeck = document.createElement('div');
+            completedDeck.className = 'cardArea mr-4 bg-secondary d-flex justify-content-center align-items-center rounded'
+            completedDeck.innerHTML = `<img src="icons/${sortColor(index)}-24px.svg" style="opacity: 0.8">`
+            completedDeck.id = `${sortColor(index)}`
+            finished.appendChild(completedDeck);
+
+        })
+    }
+
+    storefinishCard()
+
+    /*上方暫放牌區渲染*/
+    let temporary = document.getElementById('temporary')
+    function storetempCard(){
+        temporaryArea.forEach(function(item,index){
+            let temporaryDeck = document.createElement('div');
+            temporaryDeck.className = 'cardArea mr-4 bg-secondary rounded';
+            temporary.appendChild(temporaryDeck);
+        })
+    }
+
+    storetempCard();
+
+    /*拖曳效果各事件觸發函式*/
+
+    function dragStart(e) {
+        e.defaultPrevented;
+        if (isgamePause) {
+            return
+        }
+        ondragCard = e.target.card;
+        ondragGroup = e.target.group;
+        ondragSection = e.target.section;
+    };
+
+    function dragEnter(e) {
+        e.defaultPrevented;
+        // console.log(e.target)
+        if(e.target.id === 'gamingArea'){
+            return
+        }
+        if(e.target.id === 'finished'){
+            console.log('456')
+            isFinished = true
+        }
+        if(e.target.id === 'temporary'){
+            isTemporary = true
+        }
+        ondropCard = e.target.className;
+        if(ondropCard === ondragCard){
+            return
+        }
+      
+    };
+
+    function dragLeave(e){
+        if(e.target.id === ground){
+            console.log('123')
+        }
+    }
+
+
+    /*拖曳效果事件監聽*/
+    let container = document.getElementById('container');
+    container.addEventListener('dragstart', dragStart);
+    container.addEventListener('dragenter', dragEnter);
+    container.addEventListener('dragleave',dragLeave);
+    // let spadeFinish = document.getElementById('spade');
+    // let heartFinish = document.getElementById('heart');
+    // let diamondFinish = document.getElementById('diamond');
+    // let clubFinish = document.getElementById('club');
+    // spadeFinish.addEventListener('dragenter',dragEnter);
+    // heartFinish.addEventListener('dragenter',dragEnter);
+    // diamondFinish.addEventListener('dragenter',dragEnter);
+    // clubFinish.addEventListener('dragenter',dragEnter);
+
 }
 startGame();
 
